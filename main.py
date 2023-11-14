9431d1307bf8f35786cb83d1dc86c28e76358667
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-11-14 09:27:15 +0300

    Add lines

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], border=False, ln=1, align="L")
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for yl in range(20, 298, 20):
+        pdf.set_text_color(100, 100, 100)
+        pdf.line(x1=10, y1=yl, x2=200, y2=yl)
 
     # set the footer on the main page
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
     if row["Pages"] > 1:
         for i in range(row["Pages"] - 1):
             pdf.add_page()
+
+            # set the additional lines on additional pages
+            for yl in range(20, 298, 20):
+                pdf.set_text_color(100, 100, 100)
+                pdf.line(x1=10, y1=yl, x2=200, y2=yl)
 
             # set the footer on the additional pages
             pdf.ln(277)
